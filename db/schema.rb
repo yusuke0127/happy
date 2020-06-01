31d41129593e123c548f65661df1f41fca5eb08e
yusuke ishida <ishidayusuke01@gmail.com>
2020-06-01 15:29:26 +0900

    Fix conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2020_06_01_043225) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,9 +82,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_043225) do
     t.string "access_token"
     t.datetime "expires_at"
     t.string "refresh_token"
-
     t.string "line_id"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
