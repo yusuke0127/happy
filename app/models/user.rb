2a408cdf5fbaafb92b01622c6f6d3ffe7631f98c
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-01 13:52:33 +0800

    manage to create friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_friendship
   has_many :moods, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
