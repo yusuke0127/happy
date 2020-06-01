14820419f6e59532a6b66140396ed1124a5ec29b
Munekatsu Furugen <rinconlincon@gmail.com>
2020-06-01 17:23:45 +0900

    Can send sad message to friends

--- app/jobs/send_friend_message_job.rb
+++ app/jobs/send_friend_message_job.rb
@@ -1,15 +1,16 @@
 class SendFriendMessageJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id, friend_name)
+  def perform(user_id, friend_id)
     user = User.find(user_id)
+    friend = User.find(friend_id)
 
     @client ||= Line::Bot::Client.new do |config|
      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     end
 
-    text = "Hey, your friend #{friend_name} feels unhappy today... \n Send them a message to cheer them up!"
+    text = "Hey, your friend #{friend.name} is feeling awful today... \n Send them a message to cheer them up!"
     message = {
       type: 'text',
       text: text
