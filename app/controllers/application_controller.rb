d2d23077e3657926bb699a1c83307997e2fca03a
yusuke ishida <ishidayusuke01@gmail.com>
2020-06-01 20:51:56 +0900

    Added calendar and connceted the happy calendar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,27 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  protect_from_forgery with: :exception
+
+  # Starting action in config/routes.rb
+  def welcome
+
+    # Redirect to Google Authorization Page
+    redirect_to GoogleCalendar.new.auth_uri
+
+  end
+
+  def token
+    # Get a auth_client object from Google API
+    @google_api = GoogleCalendar.new
+
+    @google_api.auth_client.code = params[:code] if params[:code]
+    response = @google_api.auth_client.fetch_access_token!
+
+    session[:access_token] = response['access_token']
+
+    # Whichever Controller/Action needed to handle what comes next
+    redirect_to new_event_path()
+  end
 end
