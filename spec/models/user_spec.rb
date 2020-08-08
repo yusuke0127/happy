b0ecb6d170cb4e9bf2bd43aca8e2b6ffbfdbf923
yusuke ishida <ishidayusuke01@gmail.com>
2020-08-08 16:37:15 +0900

    Added model tests for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is invalid without an email address" do
+    user = User.new(
+      email: nil
+    )
+    expect(user).to be_invalid
+  end
+
+  it "is invalid with duplicate email" do
+    user = User.create(email: "tester@gmail.com", password: "123456")
+    other_user = User.new(email: "tester@gmail.com")
+    other_user.valid?
+    expect(other_user.errors[:email]).to include("has already been taken")
+  end
+end
